9e754eb29adf728d2badb8079ce42bd4632bb629
Lucas <lucasdalazen06@gmail.com>
2025-10-16 22:33:58 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+window.sr = ScrollReveal({reset:true});
+
+sr.reveal('.grid-conteudo', {duration: 1000});
+sr.reveal('.video', {duration: 2000});
+sr.reveal('.grid-equipe', {duration: 2100});
+sr.reveal('.grid-agenda', {duration: 1000});
